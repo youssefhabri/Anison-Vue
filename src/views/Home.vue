9d64283d8d064b610b894ae104e8bd564ca1cd81
Deterio <deterimo@gmail.com>
2019-02-16 17:45:06 +0100

    Add carousel on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,15 @@
 <template>
   <div class="home">
     <h1>Latest songs</h1>
-    <div class="uk-child-width-expand@s uk-grid-small uk-grid-match" uk-grid uk-sortable>
-      <div class="uk-card uk-card-default uk-card-body uk-margin uk-card-hover" v-for="(songs, idx) in songs.slice(0, 4)" :key="idx">
-        <div class="uk-card-media-top">
-          <img :src="songs.cover" alt="">
-        </div>
-        <h3 class="uk-card-title">{{ songs.name }}</h3>
-        <p>{{ songs.artist }}</p>
-      </div>
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 1:1; min-height: 300; max-height: 300; animation: push">
+      <ul class="uk-slideshow-items">
+          <li v-for="(songs, idx) in songs.slice(0, 4)" :key="idx">
+            <img :src="songs.cover" alt="" uk-cover>
+            <div class="uk-position-center uk-panel"><h1 class="slidetext">{{ songs.artist }} - {{ songs.name }}</h1></div>
+          </li>
+      </ul>
+      <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
+      <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
     </div>
     <aplayer/>
   </div>
@@ -35,3 +36,12 @@ export default {
   }
 }
 </script>
+
+<style >
+.slidetext, .slidetext:lang(ja-jp) {
+  font-size: 60px; 
+  font-weight: 500; 
+  -webkit-text-stroke: 1px black;
+  text-stroke: 1px black;
+}
+</style>
