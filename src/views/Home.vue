ffecc9ac05b8100e9e71d35308d3a0630c750595
Deterio <deterimo@gmail.com>
2019-02-16 18:02:58 +0100

    Add autoplay to carousel

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,13 @@
 <template>
   <div class="home">
     <h1>Latest songs</h1>
-    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 1:1; min-height: 300; max-height: 300; animation: push">
+    <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slideshow="ratio: 1:1; min-height: 300; max-height: 300; animation: push; autoplay: true; autoplay-interval: 6000; pause-on-hover: true;">
       <ul class="uk-slideshow-items">
-          <li v-for="(songs, idx) in songs.slice(0, 4)" :key="idx">
+          <li v-for="(songs, idx) in songs.slice(0, 5)" :key="idx">
             <img :src="songs.cover" alt="" uk-cover>
             <div class="uk-position-center uk-panel"><h1 class="slidetext">{{ songs.artist }} - {{ songs.name }}</h1></div>
           </li>
       </ul>
-      <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" uk-slidenav-previous uk-slideshow-item="previous"></a>
-      <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slideshow-item="next"></a>
     </div>
     <aplayer/>
   </div>
@@ -37,7 +35,7 @@ export default {
 }
 </script>
 
-<style >
+<style lang="scss" scoped>
 .slidetext, .slidetext:lang(ja-jp) {
   font-size: 60px; 
   font-weight: 500; 
