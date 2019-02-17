4e03885352178efa1cd587d99857242f3ea5a7cc
Deterio <deterimo@gmail.com>
2019-02-17 15:09:56 +0100

    Remove current Admin system and replace favicons

--- src/router.js
+++ src/router.js
@@ -7,13 +7,8 @@ import Home from '@/views/Home';
 import Login from '@/views/Login';
 import Register from '@/views/Register';
 import Play from '@/views/Play';
-import Admin from '@/views/Admin';
 
 Vue.use(Router);
-
-const admins = [
-  "HOvoQ6cWmyMyJAJ7m9uk3I8hnqg2"
-];
 
 const router = new Router({
   mode: 'history',
@@ -52,15 +47,6 @@ const router = new Router({
       meta: {
         requiresAuth: true
       }
-    },
-    {
-      path: '/admin',
-      name: 'admin',
-      component: Admin,
-      meta: {
-        requiresAuth: true,
-        requiresAdmin: true
-      }
     }
   ]
 });
@@ -76,16 +62,8 @@ Vue.use(VueAnalytics, {
 router.beforeEach((to, from, next) => {
   const currentUser = firebase.auth().currentUser;
   const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
-  var requiresAdmin = false;
-  var isAdmin = false;
-
-  if (currentUser) {
-    requiresAdmin = to.matched.some(record => record.meta.requiresAdmin);
-    isAdmin = admins.includes(currentUser.uid);
-  }
 
   if (requiresAuth && !currentUser) next('login');
-  else if (!requiresAuth && requiresAdmin && isAdmin) next('admin');
   else if (!requiresAuth && currentUser) next('home');
   else next();
 });
